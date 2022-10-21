95a019907aa6ce287d47951ccc7d7914bcde8800
imac <levvaschenko@gmail.com>
2022-10-20 19:18:02 -0700

    added animation, buy buttons, changing and fixed product counter

--- app.js
+++ app.js
@@ -7,4 +7,20 @@ $(".slider1").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     dots: true,
-  });
+});
+
+// при нажатии на кнопку buy - цифра у корзины будет увеличиваться
+
+let productsCount = document.getElementById("products-count") // getElementById потому что элемент один
+console.log(productsCount)
+
+let addToCartButtons = document.querySelectorAll(".buy-button") // querySelectorAll потому что элементов много
+console.log(addToCartButtons);
+
+addToCartButtons.forEach((item) => {
+    item.addEventListener("click", function () {
+        productsCount.textContent = +productsCount.textContent + 1;
+    });
+})
+
+AOS.init();
